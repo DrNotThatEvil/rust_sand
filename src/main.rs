a0e8188365a91046b6f9072b7b845ec4d547f119
DrNotThatEvil <wilvin@wilv.in>
2023-01-03 23:48:38 +0100

    WIP: Basic working of a chunk still WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use ggez::conf::WindowMode;
-use ggez::{Context, ContextBuilder, GameResult};
+use ggez::{Context, ContextBuilder, GameResult, timer};
 use ggez::graphics::{self, Color};
 use ggez::event::{self, EventHandler};
 
@@ -12,6 +12,11 @@ struct Chunk {
 impl Chunk {
     pub fn new(x: i32, y: i32, w: i32, h: i32) -> Chunk {
         let mut field: Vec<u8> = Vec::with_capacity((w * h) as usize);
+        for y in (0..h) {
+            for x in (0..w) {
+                field.push(0);
+            }
+        }
         
         Chunk {
             pos: (x, y),
@@ -20,10 +25,87 @@ impl Chunk {
         }
     }
 
-    pub fn update(&mut self) {
-        for y in (0..self.size.1).rev() { 
-            println!("y: {}", y);
+    pub fn update(&mut self, ctx: &mut Context) {
+        for y in (0..self.size.1) { 
+            for x in 0..self.size.0 {
+                println!("X: {}, Y: {}", x, y);
+
+                let cur_index: usize =  (x + (y * self.size.0)) as usize;
+                let cell: u8 = self.field[cur_index];
+                
+                if cell > 0 {
+                    if y < (self.size.1-1) {
+                        // not last row so moving down allowed!
+                        let below_index: usize = (x + ((y+1) * self.size.0)) as usize;
+                        let cell_below: u8 = self.field[below_index];
+                        
+                        if cell_below == 0 {
+                            // Move below
+                            self.field.swap(cur_index, below_index);
+                            continue;
+                        }
+
+                        if x > 0 {
+                            // Can move left 
+                            let below_l_index: usize = ((x-1) + ((y+1) * self.size.0)) as usize;
+                            let cell_l_below: u8 = self.field[below_l_index];
+
+                            if cell_l_below == 0 {
+                                // Move left below
+
+                                self.field.swap(cur_index, below_l_index);
+                                continue;
+                            }
+                        }
+
+                        if x < (self.size.0 - 1) {
+                            // Can move right
+                            let below_r_index: usize = ((x+1) + ((y+1) * self.size.0)) as usize;
+                            let cell_r_below: u8 = self.field[below_r_index];
+
+                            if cell_r_below == 1 {
+                                // Move right below 
+
+                                self.field.swap(cur_index, below_r_index);
+                                continue;
+                            }
+                        }
+                    }
+                }
+
+                if cell == 0 && x == 50 && y == 0 {
+                    if(timer::check_update_time(ctx, 60)) {
+                        self.field[cur_index] = 1;
+                    }
+                }
+            }
         }
+    }
+
+    fn draw(&self, ctx: &mut Context) -> GameResult<graphics::Mesh> {
+        let mb = &mut graphics::MeshBuilder::new();
+
+        for y in 0..self.size.1 { 
+            for x in 0..self.size.0 {
+                let pos = (self.pos.0 + x, self.pos.1 + y);
+                
+                let cur_index: usize =  (x + (y * self.size.0)) as usize;
+                let cell: u8 = self.field[cur_index];
+
+                if cell > 0 {
+                    mb.rectangle(
+                        graphics::DrawMode::fill(),
+                        graphics::Rect::new_i32(pos.0, pos.1, 1, 1),
+                        graphics::Color::new(1.0, 0.0, 0.0, 1.0),
+                    )?;  
+                }
+                
+
+            }
+        }
+        
+         
+        Ok(graphics::Mesh::from_data(ctx, mb.build()))
     }
 }
 
@@ -48,7 +130,7 @@ impl EventHandler for GGSand {
         // Update code here...
 
         for chunk in self.chunks.iter_mut() {
-            chunk.update();
+            chunk.update(ctx);
         }
 
         Ok(())
@@ -56,8 +138,18 @@ impl EventHandler for GGSand {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_frame(ctx, Color::BLACK);
-        // Draw code here...
-        canvas.finish(ctx)
+
+        for chunk in self.chunks.iter() {
+            let chunk_mesh: graphics::Mesh = chunk.draw(ctx)?;
+            canvas.draw(
+                &chunk_mesh,
+                graphics::DrawParam::new()
+            );
+        }
+
+        canvas.finish(ctx)?;
+
+        Ok(())
     }
 }
 
